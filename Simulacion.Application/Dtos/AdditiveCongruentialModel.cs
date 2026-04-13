a9fe2309a1eed3cd089a594d6ecc9f24545a5aec
Ignacio Veliz <ignaciomartin.veliz@alu.frt.utn.edu.ar>
2026-04-13 01:56:45 -0300

    feat: add additive congruential generator for pseudo-random numbers

---
+++ Simulacion.Application/Dtos/AdditiveCongruentialModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulacion.Application.Dtos;
+
+public record AdditiveCongruentialModel(long M,long N0,long N1,int TotalNumeros);
+
