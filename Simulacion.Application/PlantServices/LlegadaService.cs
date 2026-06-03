f6c03205fe2987b886d8f4505b9c4fcdd2fbedac
ignaveliz <ignaciomartin.veliz@alu.frt.utn.edu.ar>
2026-06-03 00:20:03 -0300

    Aumentar dispositivos y proporcion de pantallas para experimentacion

--- Simulacion.Application/PlantServices/LlegadaService.cs
+++ Simulacion.Application/PlantServices/LlegadaService.cs
@@ -1,4 +1,4 @@
-﻿using Simulacion.Domain.Entities;
+using Simulacion.Domain.Entities;
 using Simulacion.Domain.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -25,8 +25,8 @@ namespace Simulacion.Application.PlantServices
 
         public ResultadoCamioneta ProcesarNuevaCamioneta()
         {
-            int cantDispositivos = (int)_distribuciones.GenerarNormal(50, 10);
-            int cantTVMonitores = (int)_distribuciones.GenerarBinomial(cantDispositivos, 0.095);
+            int cantDispositivos = (int)_distribuciones.GenerarNormal(150, 30);
+            int cantTVMonitores = (int)_distribuciones.GenerarBinomial(cantDispositivos, 0.30);
 
             /*Probabilidad de que estas TV/Monitores sean aptos vs descartados (90-10)*/
             int cantAptos = (int)_distribuciones.GenerarBinomial(cantTVMonitores, 0.9);
